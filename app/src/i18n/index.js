533cd0af07b00b4c41920f6276b94ea9f7dc8b80
Iván Chicano <ivchicano@gmail.com>
2021-04-27 10:31:46 +0200

    import missing languages in i18n

--- app/src/i18n/index.js
+++ app/src/i18n/index.js
@@ -1,5 +1,13 @@
 import enUS from './en-us'
+import it from './it'
+import de from './de'
+import es from './es'
+import nl from './nl'
 
 export default {
-  'en-us': enUS
+  'en-us': enUS,
+  it,
+  de,
+  es,
+  nl
 }
